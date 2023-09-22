c83af66c1fbec4d6cd0b61dcd9649e13f440284d
MichaelYKersey <144640226+MichaelYKersey@users.noreply.github.com>
2023-09-22 09:46:01 -0700

    Wire Library request
    based off of: https://mschoeffler.com/2017/10/05/tutorial-how-to-use-the-gy-521-module-mpu-6050-breakout-board-with-the-arduino-uno/

--- Controls-Trial-Project.ino
+++ Controls-Trial-Project.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+#include <Wire.h>
 
 Servo gimbalServo;
 
@@ -12,4 +13,17 @@ void setup() {
 void loop() {
   int pos = 90;// TODO update servo based on MPU
   gimbalServo.write(pos);
+  delay(100);
+  Serial.println("Temp:"+ (String)(getDPMData(0x41,2)/340.00+36.53));
+}
+int getDPMData(byte dataAdrees, int bytesRequested) {
+  Wire.beginTransmission(0x68);
+  Wire.write(dataAdrees);
+  Wire.endTransmission(false);
+  Wire.requestFrom(0x68,bytesRequested,true);
+  int bytes = Wire.read();
+  for (int i = 1; i< bytesRequested; i++){
+    bytes = (bytes << 8) + Wire.read();
+  }
+  return bytes;
 }
