08882d3c7de468e45ebeb3573d88630f90d58344
MichaelYKersey <144640226+MichaelYKersey@users.noreply.github.com>
2023-09-30 21:42:04 -0700

    drift removal

--- Controls-Trial-Project.ino
+++ Controls-Trial-Project.ino
@@ -15,7 +15,8 @@ const byte kXGyroOutAdress = 0x43;
 const byte kGyroConfigAdress = 0x1B;
 const byte kGyroConfig = 0b00000000;
 const byte kPowerManagement1Adress = 0x6B;
-const double kGyroBitsToDegPerSec = 1/131.0; //131 is the LSB/deg/sec for The Gyro
+const double kGyroBitsToDegPerSec = (1<<0)/131.0; //131 is the LSB/deg/sec for The Gyro
+double drift;
 
 void setup() {
   Serial.begin(9600);// Bit send rate for Serial port
@@ -37,23 +38,34 @@ void setup() {
   Wire.write(0b00000000);
   Wire.endTransmission(true);
 
+  delay(100);
+
   //config MPU
   Wire.beginTransmission(kMPUAdress);
   Wire.write(kGyroConfigAdress);
   Wire.write(kGyroConfig);
   Wire.endTransmission(true);
+
+  delay(100);
+
+  Serial.println("Starting Calibration");
+  setDrift();
+  Serial.println("Calibration Finished");
+  delay(3000);
+
+  prevTime = millis(); 
 }
 
 void loop() {
+  updateRoll();
   int pos = (int)rollDeg + 90;// TODO update servo based on MPU
   if (pos < 0) pos=0;
   if (pos > 180) pos=180;
   //pos=90;
   gimbalServo.write(pos);
-  updateRoll();
-  if (i==10000) {
-    Serial.println("Temp:"+ (String)(getDPMData(kTempetureRegesterAdress,2)/340.00+36.53));
-    Serial.println("XGyro:"+ (String)(getDPMData(kXGyroOutAdress,2) * kGyroBitsToDegPerSec));
+  if (i==100) {
+    // Serial.println("Temp:"+ (String)(getDPMData(kTempetureRegesterAdress,2)/340.00+36.53));
+    // Serial.println("XGyro:"+ (String)(getDPMData(kXGyroOutAdress,2) * kGyroBitsToDegPerSec-drift));
     Serial.println("Roll:"+ (String)(rollDeg));
     i=0;
   }
@@ -67,9 +79,11 @@ void updateRoll() {
   // }
   unsigned long time = millis();
   double timeDeltaSec = (time - prevTime) / 1000.0; // diffrence in time in seconds
-  double rollVel = getDPMData(kXGyroOutAdress,2) * kGyroBitsToDegPerSec;
-  rollDeg += rollVel * timeDeltaSec;
   prevTime = time;
+
+  int messurement = getDPMData(kXGyroOutAdress,2);
+  double rollVel = messurement * kGyroBitsToDegPerSec - drift;
+  rollDeg += rollVel * timeDeltaSec;
 }
 
 int getDPMData(byte dataAdrees, int bytesRequested) {
@@ -82,4 +96,19 @@ int getDPMData(byte dataAdrees, int bytesRequested) {
     bytes = (bytes << 8) + Wire.read();
   }
   return bytes;
+}
+
+void setDrift(){
+  // int start = millis();
+  int count =0;
+  double rollVelTotal = 0;
+  // while (start + 10000 > millis()) {
+  for (int i=0; i < 1000; i++) {
+    delay(3);
+    rollVelTotal += getDPMData(kXGyroOutAdress,2) * kGyroBitsToDegPerSec;
+    count++;
+  }
+  drift = rollVelTotal/count;
+  Serial.println("Drift:"+ (String)(drift));// + "," + (String)(count));
+  
 }
